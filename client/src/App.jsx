6a512f4419a02b0497e4588bfa493099e89c2fd9
YatinDora81 <yatin.dora81@gmail.com>
2025-06-14 11:53:13 +0530

    okay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
 
   return (
     <div>
-      <h1>poiuytrety✏️</h1>
+      <h1>This is app for testing purpose of MERN STACK CI-CD</h1>
       <h1>API REQUEST IS {apiClick}</h1>
       <button style={{ marginTop: "20px" }} onClick={setCookieHandler}>
         {" "}
