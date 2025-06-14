a9b84908fe1de528d862eb75c761e4465fd1ecbb
YatinDora81 <yatin.dora81@gmail.com>
2025-06-14 11:39:23 +0530

    okay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello from Yatin? :":"</h1>
+      <h1>This is for testing purpose</h1>
       <h1>API REQUEST IS {apiClick}</h1>
       <button style={{ marginTop: "20px" }} onClick={setCookieHandler}>
         {" "}
